7c2442482aba3638db31374689f5c6f4ce740d1c
gaojun <gaojun1990124@126.com>
2016-11-22 21:46:47 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Http}from '@angular/http'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app over!';
+}
