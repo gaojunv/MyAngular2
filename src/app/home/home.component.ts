6b242e1b8c3cf3f10a6641b92b4b3d4a5e14df69
gaojun <gaojun@fcsc.com>
2017-02-13 13:42:18 +0800

    修改

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,30 +5,30 @@ import { UserService } from '../services';
     templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-   token:string;
-   users:any[];
-   oldUsers:any[];
-   cols: any[];
+    token: string;
+    users: any[];
+    oldUsers: any[];
+    cols: any[];
     constructor(private userService: UserService) {
         this.token = localStorage.getItem('token');
     }
 
     ngOnInit() {
-        this.userService.getUserList().then(users => {this.users = users;this.oldUsers = JSON.parse(JSON.stringify(this.users))});
+        this.userService.getUserList().then(users => { this.users = users; this.oldUsers = JSON.parse(JSON.stringify(this.users)) });
         this.cols = [
-            {field: 'username', header: '用户名', sortable: 'true'},
-            {field: 'email', header: '邮箱', sortable: 'true'},
-            {field: 'firstname', header: '姓氏', sortable: 'true'},
-            {field: 'lastname', header: '名字', sortable: 'true'}
+            { field: 'username', header: '用户名', sortable: true, editable: true },
+            { field: 'email', header: '邮箱', sortable: false, editable: true },
+            { field: 'firstname', header: '姓氏', sortable: false, editable: false },
+            { field: 'lastname', header: '名字', sortable: true, editable: false }
         ];
     }
 
-    showUsers(){
+    showUsers() {
         console.log(this.users);
         console.log(this.oldUsers);
     }
 
-    resetUsers(){
+    resetUsers() {
         this.users = JSON.parse(JSON.stringify(this.oldUsers));
     }
 }
